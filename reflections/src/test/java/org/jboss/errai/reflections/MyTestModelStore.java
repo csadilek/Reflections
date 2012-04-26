a26051db217e38a226b5f65f98bed6b738ddbb1b
Jonathan Fuerth <jfuerth@gmail.com>
2012-04-26 15:37:22 -0400

    Back to 2.0-SNAPSHOT

--- reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
+++ reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
@@ -1,4 +1,4 @@
-//generated using Reflections JavaCodeSerializer [Thu Apr 19 12:46:40 EDT 2012]
+//generated using Reflections JavaCodeSerializer [Thu Apr 26 15:35:57 EDT 2012]
 package org.jboss.errai.reflections;
 
 import static org.jboss.errai.reflections.serializers.JavaCodeSerializer.*;
